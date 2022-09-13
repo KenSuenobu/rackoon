6cf353327a3d3c3fe1f4e665ee0ff89caf16e6e9
Ken Suenobu <ksuenobu@fastmail.com>
2022-09-12 20:29:34 -0600

    added server binary

---
+++ bin/server.ts
@@ -0,0 +1,25 @@
+/**
+ * Open-DCMon
+ */
+
+import {OpenDCController} from "../src/api";
+import {OpenDCRouter} from "../src/routes/OpenDCRouter";
+
+(async () => {
+
+    const express = require('express');
+    const app = express();
+    const bodyParser = require('body-parser');
+
+    app.use(bodyParser.json());
+
+    const port = 3000;
+    const controller: OpenDCController = new OpenDCController();
+
+    OpenDCRouter.registerAll(app, controller);
+
+    app.listen(port, () => {
+        console.log(`Accepting connections on: http://localhost:${port}/`);
+    });
+
+})();
