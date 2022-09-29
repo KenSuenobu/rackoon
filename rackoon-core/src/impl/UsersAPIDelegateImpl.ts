12a42a3eb59915f31fc0ba662bc98caaf18bb596
Ken Suenobu <ksuenobu@fastmail.com>
2022-09-28 23:01:10 -0600

    added user SQL

--- rackoon-core/src/impl/UsersAPIDelegateImpl.ts
+++ rackoon-core/src/impl/UsersAPIDelegateImpl.ts
@@ -14,6 +14,23 @@ export class UsersAPIDelegateImpl extends UsersAPIDelegate {
    * Response codes: 201, 401
    */
   async createUser(request: { payload?: User }): Promise<void> {
+    if (!request.payload) {
+      throw new Error("No user payload was given.");
+    }
+
+    this.dbInstance.run(
+      "INSERT INTO user (name, username, password, email_address, phone_number, is_admin) " +
+        "VALUES ($name, $username, $password, $email_address, $phone_number, $is_admin)",
+      {
+        $name: request.payload.name,
+        $username: request.payload.username,
+        $password: request.payload.password,
+        $email_address: request.payload.emailAddress,
+        $phone_number: request.payload.phoneNumber,
+        $is_admin: request.payload.isAdmin,
+      }
+    );
+
     return;
   }
 
